a5a2ecca7e99950407b04914fa169927792e8f8e
kurohishi <77273148+kurohishi@users.noreply.github.com>
2024-12-02 22:43:18 +0900

    Update BulletShotSimulator.cs

--- Assets/Script/BulletShotSimulator.cs
+++ Assets/Script/BulletShotSimulator.cs
@@ -5,25 +5,25 @@ public class BulletShotSimulator : MonoBehaviour
     [SerializeField] private ChanonController _cc;
     void Start()
     {
-        CreatePhysicsScene(); // 開始時に仮想シーンを作る
+        CreatePhysicsScene(); 
     }
     private void Update()
     {
-        Simulation(m_ballPrefab,transform.position - Vector3.up, transform.forward * _cc.bulletVellocity); // シミュレーションの結果を更新する
+        Simulation(m_ballPrefab, transform.position - Vector3.up, transform.forward * _cc.bulletVellocity);
     }
 
-    private Scene m_simulationScene; // 仮想シーン
-    private PhysicsScene m_physicsScene; // 物理シーン
+    private Scene m_simulationScene;
+    private PhysicsScene m_physicsScene;
     [SerializeField] private Transform m_obstacleParent;
 
     private void CreatePhysicsScene()
     {
-        m_simulationScene = SceneManager.CreateScene("Simulation", new CreateSceneParameters(LocalPhysicsMode.Physics3D));
+        m_simulationScene = SceneManager.CreateScene("Simulation", new CreateSceneParameters(LocalPhysicsMode.Physics3D)); 
         m_physicsScene = m_simulationScene.GetPhysicsScene();
 
         foreach (Transform tf in m_obstacleParent)
         {
-            var ghost = Instantiate(tf.gameObject, tf.position, tf.rotation);
+            var ghost = Instantiate(tf.gameObject, tf.position, tf.rotation); 
             ghost.GetComponent<Renderer>().enabled = false;
             SceneManager.MoveGameObjectToScene(ghost, m_simulationScene);
         }
@@ -45,9 +45,10 @@ public class BulletShotSimulator : MonoBehaviour
         for (int i = 0; i < m_iMaxPhysicsFrame; i++)
         {
             m_physicsScene.Simulate(Time.fixedDeltaTime);
-            m_line.SetPosition(i,ghost.transform.position);
+            m_line.SetPosition(i, ghost.transform.position);
         }
 
         Destroy(ghost.gameObject);
     }
 }
+
